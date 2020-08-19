329d88f07f2b86f2e47c0efad972ac1741441b33
Jessica Nguyen <jessicaawhy@gmail.com>
2020-08-18 22:02:35 -0700

    First Pass
    Initialize project, created skeleton files, added a few basic functions that are not complete but render fake data.

---
+++ main.js
@@ -0,0 +1,45 @@
+
+// const submit = document.querySelector('#submit');
+// submit.addEventListener('submit', addBookToLibrary);
+
+
+let myLibrary = [{
+  title: 'title',
+  author: 'author',
+  pages: 5,
+  status: 'status',
+},{
+  title: 'title',
+  author: 'author',
+  pages: 3,
+  status: 'status',
+}];
+
+// function Book(title, author, pages, status) {
+
+// }
+
+function addBookToLibrary() {
+  let title = document.getElementById('title').value;
+  let author = document.getElementById('author').value;
+  let pages = document.getElementById('pages').value;
+  let status = document.getElementById('status').value;
+
+  let currentBook = {
+    title: title,
+    author: author,
+    pages: pages,
+    status: status,
+  }
+  
+  myLibrary.push(currentBook);
+}
+
+function render(array) {
+  const container = document.querySelector('#container');
+  array.forEach(item => {
+    let book = document.createElement('div');
+    book.textContent = JSON.stringify(item);
+    container.appendChild(book);
+  })
+}
