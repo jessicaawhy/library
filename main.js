52b1689d305a88af3d01696a36c40895e0f56b58
Jessica Nguyen <jessicaawhy@gmail.com>
2020-08-20 11:34:15 -0700

    Add functionality to the remove book button

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
-let myLibrary = [{
-  title: 'It',
-  author: 'Stephen King',
-  pages: 1138,
-  status: 'Read',
-},{
-  title: 'Harry Potter and the Sorcerer\'s Stone',
-  author: 'J. K. Rowling',
-  pages: 309,
-  status: 'Not Read',
-}];
-
 // function Book(title, author, pages, status) {
 // }
-
 
 function addBookToLibrary() {
   let title = document.getElementById('title').value;
@@ -26,29 +13,29 @@ function addBookToLibrary() {
   }
 
   if (title && author && pages && status) {
-    
     let currentBook = {
       title: title,
       author: author,
       pages: pages,
       status: status, 
     }
-    
     myLibrary.push(currentBook);
     form.reset();
     toggleDisplay();
-
   } else {
     alert('Please fill out all fields.');
   }
 }
 
-function render(array) {
+function removeChild() {
   const container = document.querySelector('#container');
   while(container.firstChild) {
     container.removeChild(container.firstChild);
   }
+}
 
+function createChildElements(array) {
+  const container = document.querySelector('#container');
   array.forEach((item, index) => {
     let book = document.createElement('tr');
 
@@ -58,6 +45,7 @@ function render(array) {
     let status = document.createElement('td');
     let deleteButton = document.createElement('td');
     let button = document.createElement('button');
+    button.classList.add('delete-button');
     button.setAttribute('id', index);
     button.textContent = "x";
 
@@ -72,20 +60,68 @@ function render(array) {
     book.appendChild(pages);
     book.appendChild(status);
     book.appendChild(deleteButton);
-    
 
     container.appendChild(book);
   })
+}
+
+function render(array) {
+  removeChild();
+  createChildElements(array);
+  createDeleteListeners(array);
+}
+
+function createDeleteListeners(array) {
+  const buttons = document.querySelectorAll('.delete-button');
+  buttons.forEach(button => button.addEventListener(('click'), () => {
+    const index = button.getAttribute('id');
+    array.splice(index, 1);
+    render(array);
+  }))
 }
 
 const addBook = document.querySelector('#add-book');
 const form = document.querySelector('#new-book-form');
 addBook.addEventListener('click', toggleDisplay);
 
-
 function toggleDisplay() {
   form.classList.toggle('toggle-visibility');
 }
 
 // initial
+
+let myLibrary = [{
+  title: 'It',
+  author: 'Stephen King',
+  pages: 1138,
+  status: 'Read',
+},{
+  title: "Harry Potter and the Sorcerer's Stone",
+  author: 'J. K. Rowling',
+  pages: 309,
+  status: 'Read',
+},
+{
+  title: 'Eloquent JavaScript',
+  author: 'Marijin Haverbeke',
+  pages: 448,
+  status: 'Not Read',
+},{
+  title: 'The Subtle Art of Not Giving a F*ck',
+  author: 'Mark Manson',
+  pages: 224,
+  status: 'Not Read',
+},
+{
+  title: 'The Hate U Give',
+  author: 'Angie Thomas',
+  pages: 444,
+  status: 'Not Read',
+},{
+  title: 'The Gift of Fear',
+  author: 'Gavin de Becker',
+  pages: 432,
+  status: 'Not Read',
+}];
+
 render(myLibrary);
