754730debc8968359d26b12796f8767762e828ac
Jessica Nguyen <jessicaawhy@gmail.com>
2020-08-20 15:13:52 -0700

    Add toggle for read status

--- main.js
+++ main.js
@@ -41,33 +41,32 @@ function createChildElements(array) {
     let title = document.createElement('td');
     let author = document.createElement('td');
     let pages = document.createElement('td');
-    let status = document.createElement('td');
-    let deleteButton = document.createElement('td');
-    let button = document.createElement('button');
-    button.classList.add('delete-button');
-    button.setAttribute('id', index);
-    button.textContent = "x";
+
+    let statusElement = document.createElement('td');
+    let statusButton = document.createElement('button');
+    statusButton.classList.add('status-button');
+    statusButton.textContent = item.status;
+    statusElement.appendChild(statusButton);
+
+    let deleteElement = document.createElement('td');
+    let deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-button');
+    deleteButton.setAttribute('id', index);
+    deleteButton.textContent = "x";
+    deleteElement.appendChild(deleteButton);
 
     title.textContent = item.title;
     author.textContent = item.author;
     pages.textContent = item.pages;
-    status.textContent = item.status;
-    deleteButton.appendChild(button);
 
     book.appendChild(title);
     book.appendChild(author);
     book.appendChild(pages);
-    book.appendChild(status);
-    book.appendChild(deleteButton);
+    book.appendChild(statusElement);
+    book.appendChild(deleteElement);
 
     container.appendChild(book);
   })
-}
-
-function render(array) {
-  removeChild();
-  createChildElements(array);
-  createDeleteListeners(array);
 }
 
 function createDeleteListeners(array) {
@@ -79,6 +78,27 @@ function createDeleteListeners(array) {
   }))
 }
 
+function createReadListeners(array) {
+  const buttons = document.querySelectorAll('.status-button');
+  buttons.forEach((button, index) => button.addEventListener('click', () => {
+    if (array[index].status === "Read") {
+      array[index].status = "Not Read";
+    } else {
+      array[index].status = "Read";
+    }
+    render(array);
+  }))
+}
+
+function render(array) {
+  removeChild();
+  createChildElements(array);
+  createDeleteListeners(array);
+  createReadListeners(array);
+}
+
+
+
 const addBook = document.querySelector('#add-book');
 const form = document.querySelector('#new-book-form');
 addBook.addEventListener('click', toggleDisplay);
@@ -89,38 +109,20 @@ function toggleDisplay() {
 
 // initial
 
-let myLibrary = [{
-  title: 'It',
-  author: 'Stephen King',
-  pages: 1138,
-  status: 'Read',
-},{
-  title: "Harry Potter and the Sorcerer's Stone",
-  author: 'J. K. Rowling',
-  pages: 309,
-  status: 'Read',
-},
-{
-  title: 'Eloquent JavaScript',
-  author: 'Marijin Haverbeke',
-  pages: 448,
-  status: 'Not Read',
-},{
-  title: 'The Subtle Art of Not Giving a F*ck',
-  author: 'Mark Manson',
-  pages: 224,
-  status: 'Not Read',
-},
-{
-  title: 'The Hate U Give',
-  author: 'Angie Thomas',
-  pages: 444,
-  status: 'Not Read',
-},{
-  title: 'The Gift of Fear',
-  author: 'Gavin de Becker',
-  pages: 432,
-  status: 'Not Read',
-}];
+let myLibrary = [];
+
+// Populate library with data
+let book1 = new Book("It", "Stephen King", 1138, "Read");
+myLibrary.push(book1);
+let book2 = new Book("Harry Potter and the Sorcerer's Stone", "J. K. Rowling", 309, "Not Read");
+myLibrary.push(book2);
+let book3 = new Book("Eloquent JavaScript", "Marijin Haverbeke", 448, "Not Read");
+myLibrary.push(book3);
+let book4 = new Book("The Subtle Art of Not Giving a F*ck", "Mark Manson", 224, "Not Read");
+myLibrary.push(book4);
+let book5 = new Book("The Hate U Give", "Angie Thomas", 444, "Not Read");
+myLibrary.push(book5);
+let book6 = new Book("The Gift of Fear", "Gavin de Becker", 432, "Read");
+myLibrary.push(book6);
 
 render(myLibrary);
